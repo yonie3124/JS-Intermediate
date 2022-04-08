fa04cbba404b3ad5703d584025824b78ccd806b2
Yonas <98472333+yonie3124@users.noreply.github.com>
2022-04-08 15:32:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -54,16 +54,14 @@ for (let i = 1; i <= 135; i++) {
 // 0 1 1 2 3 5 8 13 21...
 
 
-let n1 = 0, n2 = 1, nextTerm;
-
-console.log('Fibonacci Series:');
-
-for (let i = 1; i <= 10; i++) {
-    console.log(n1),     
-    nextTerm = n1 + n2;
-    n1 = n2;
-    n2 = nextTerm;
+let fibonacci =[0,1];
+let sum =1;
+for (let i = 2; i < 10; i++) {
+    fibonacci[i] = fibonacci[i-2] + fibonacci[i-1];
+    sum +=fibonacci[i];
 }
+console.log(fibonacci);
+console.log(sum);
 
 // 5. Remove the Negative
 
